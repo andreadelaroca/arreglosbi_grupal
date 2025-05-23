48c6be8082923b3c8118108c4f4742bd5c8e85e9
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-05-23 15:50:03 -0600

    Validación de datos a través de función validar con def

--- ejercicio4.py
+++ ejercicio4.py
@@ -2,3 +2,14 @@
 #   a. suma de valores por fila
 #   b. promedio de valores por columna
 #   c. mayor valor almacenado en toda la matriz y su posición
+
+print("Ingrese el valor de n y m (ambos positivos y menores que 10):")
+
+# definir validación de n y m (enteros positivos y menores que 10)
+def validar(mensaje):
+    while True:
+        num = int(input(mensaje))
+        if 0 < num < 10:
+            return num
+        print("Valor inválido. Debe ser un número positivo menor que 10.\n")
+        
