917da2f1058fd8f60f6f77c9768668c3aec594f8
FICT1 <ficallejas@uamv.edu.ni>
2025-05-23 10:33:45 -0600

    Instrucciones

---
+++ ejercicio1.py
@@ -0,0 +1,23 @@
+# 1. La Abarrotera ABSA tiene 4 sucursales en las cuales se realizaron diferentes ventas en los
+# meses de Julio a diciembre del año 2022, se le ha solicitado a usted realizar un programa en
+# donde pueda capturar la siguiente tabla de datos:
+
+# Estado de cuenta de las Sucursales ABSA en el segundo semestre 2022
+# Tienda/Mes Julio  Agosto Septiembre Octubre Noviembre Diciembre
+# ABSA1      50,000 60,000  65,000    62,000   78,000    95,000
+# ABSA2      89,000 90,000  98,000    80,000   85,000    90,000
+# ABSA3      65,000 72,000  85,000    72,000   83,000    98,000
+# ABSA4      92,000 88,000  90,000    76,000   82,000    93,000
+# y nos presente los siguientes resultados:
+
+# a. Venta total por todas las tiendas
+# b. Venta total por tienda
+# c. Tienda que más vendió en los 6 meses
+# d. Tienda que menos vendió
+
+
+
+
+
+#Franklin Callejas
+
