13136d77963b367db899b08efb8d6aaf699b46d9
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-05-23 15:49:10 -0600

    Agregué el ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,4 @@
+# ingresar cada uno de los valores de la matriz, luego mostrar:
+#   a. suma de valores por fila
+#   b. promedio de valores por columna
+#   c. mayor valor almacenado en toda la matriz y su posición
