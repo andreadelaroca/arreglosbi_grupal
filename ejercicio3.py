ff1b3921e5ab75e5d4a1edd7cecea8d32a90a703
Naza2006 <nazario@uamv.edu.ni>
2025-05-23 12:53:22 -0600

    avance del codigo

--- ejercicio3.py
+++ ejercicio3.py
@@ -7,3 +7,13 @@ def linealizar_por_columnas(matriz):
     columnas = len(matriz[0]) # Obtenemos la cantidad de columnas de la matriz
     resultado = [] # Lista vacía donde vamos a almacenar los elementos linealizados
     
+    for columna in range(columnas):  # Recorremos cada columna primero
+        for fila in range(filas):    # Por cada columna, recorremos todas las filas
+            resultado.append(matriz[fila][columna])  # Agregamos el elemento de la posición [fila][columna] a la lista resultado
+            
+    return resultado
+matriz = [
+    [1, 2, 3, 7],
+    [4, 5, 6, 8]
+]
+   
